f12002f4f75424c0afd6a335dc8d3788b3747d86
diego francisco <diegofr4x@gmail.com>
2022-05-19 23:07:30 -0300

    create functions

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
-console.log("hello world!")
+const txt = document.getElementById('text');
+const btn = document.getElementById('btn');
+const res = document.getElementById('res');
+
+const arr = JSON.parse(localStorage.getItem('todo'))|| [];
+
+function init(){
+    res.innerHTML = ""
+    for(todos of arr){
+        res.innerHTML += `<p>${todos}</p>`
+    }
+}
+
+init()
+
+function addTodo(){
+    var texto = txt.value
+    arr.push(texto)
+    txt.value = ""
+    init()
+    save()
+};
+
+function save(){
+   localStorage.setItem('todo', JSON.stringify(arr))
+};
+
+btn.onclick = addTodo
